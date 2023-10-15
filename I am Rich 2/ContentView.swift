d8aa82009bd9d30354f1d44669cd6e6f900cf670
Rudolf <ronaldo679@gmail.com>
2023-10-15 18:35:04 +0300

    Initial Commit

---
+++ I am Rich 2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  I am Rich 2
+//
+//  Created by Rudolf Amiryan on 15.10.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ I am Rich 2/I_am_Rich_2App.swift
@@ -0,0 +1,17 @@
+//
+//  I_am_Rich_2App.swift
+//  I am Rich 2
+//
+//  Created by Rudolf Amiryan on 15.10.23.
+//
+
+import SwiftUI
+
+@main
+struct I_am_Rich_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
