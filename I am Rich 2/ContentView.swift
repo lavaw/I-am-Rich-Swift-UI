60749e65c74379e9bdff0ef9e0031b802a73fb4c
Rudolf <ronaldo679@gmail.com>
2023-10-17 00:25:01 +0300

    Building test app

--- I am Rich 2/ContentView.swift
+++ I am Rich 2/ContentView.swift
@@ -9,15 +9,24 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            ZStack {
+                Color(.systemTeal)
+                    .edgesIgnoringSafeArea(.all)
+                VStack {
+                    Text("I Am Rich")
+                        .font(.system(size: 40))
+                        .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                    Image("diamond")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 200, height: 200, alignment: .center)
+                        
+                }
+            }
         }
-        .padding()
     }
-}
+
 
 #Preview {
     ContentView()
